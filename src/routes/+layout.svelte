b68e77d5403308c90c30d338e07583e666950e84
cezar <doncezart>
2024-02-24 13:10:11 +0000

    Original push

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script>
+    import '@fortawesome/fontawesome-free/css/all.min.css'
+</script>
+
+<div class="slot"><slot/></div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<div class="inside">
+    <div class="personal">
+        <h1 class="title">Ținiu Cezar Gabriel</h1>
+        <p>22 years old from Romania</p>
+        <button class="btn border mail" style="width: 100%" type="button" onclick="window.location.href='mailto:tiniu@ceza.ro'"><i class="fa-solid fa-envelope icon"></i>tiniu@ceza.ro</button>
+        <div class="buttons">
+            <button class="btn filled" type="button" onclick="window.location.href='https://instagram.com/cezar.ique'"><i class="fa-brands fa-instagram icon"></i>cezar.ique</button>
+            <button class="btn filled" type="button" onclick="window.location.href='https://youtube.com/@graphic_design'"><i class="fa-brands fa-youtube icon"></i>cezar.ique</button>
+            <button class="btn filled" type="button" onclick="window.location.href='https://tiktok.com/@cezar.ique'"><i class="fa-brands fa-tiktok icon"></i>cezar.ique</button>
+            <button class="btn filled" type="button" onclick="window.location.href='https://reddit.com/user/goldieczr'"><i class="fa-brands fa-reddit-alien icon"></i>goldieczr</button>
+            <button class="btn filled" type="button" onclick="window.location.href='https://pinterest.com/goldieczr'"><i class="fa-brands fa-pinterest icon"></i>goldieczr</button>
+            <button class="btn filled" type="button" onclick="window.location.href='https://t.me/goldieczr'"><i class="fa-brands fa-telegram icon"></i>goldieczr</button>
+            <button class="btn filled" type="button" onclick="window.location.href='https://www.facebook.com/doncezarr/'"><i class="fa-brands fa-facebook-f icon"></i>doncezarr</button>
+            <button class="btn filled" type="button" onclick="window.location.href='https://discordapp.com/users/462052434563563522'"><i class="fa-brands fa-discord icon"></i>doncezart</button>
+            <button class="btn filled" type="button" onclick="window.location.href='https://don-cezar.tumblr.com'"><i class="fa-brands fa-tumblr icon"></i>don-cezar</button>
+            <button class="btn filled" type="button" onclick="window.location.href='https://steamcommunity.com/id/doncezar/'"><i class="fa-brands fa-steam icon"></i>doncezar</button>
+            <button class="btn filled" type="button" onclick="window.location.href='https://github.com/doncezart'"><i class="fa-brands fa-github icon"></i>doncezart</button>
+            <button class="btn filled" type="button" onclick="window.location.href='https://www.snapchat.com/add/parfumulafemei'"><i class="fa-brands fa-snapchat icon"></i>parfumulafemei</button>
+        </div>
+    </div>
+
+</div>
