aa05f29371ff0ce8e333caefd48f2ae1083fb724
cezar <doncezart>
2024-08-24 09:11:35 +0000

    Reworked grid gallery

--- src/routes/samp/+page.svelte
+++ src/routes/samp/+page.svelte
@@ -1,8 +1,19 @@
 <script>
   import images from '$lib/output.json';
   import { tick } from 'svelte'
+  import { MasonryGrid } from "@egjs/svelte-grid";
+
+  const gap = 10;
+  const defaultDirection = "end";
+  const align = "masonry";
+  const column = 0;
+  const columnSize = 350;
+  const columnSizeRatio = 0;
   let gallery
   let initiator
+
+
+
   async function setGallery(e) {
       console.log(e)
       if (e.newState === 'open') {
@@ -23,18 +34,28 @@
   }
 </script>
 
-
-<section id="images-wrapper" bind:this={gallery}>
+<MasonryGrid
+  class="container"
+  {defaultDirection}
+  {gap}
+  {align}
+  {column}
+  {columnSize}
+  {columnSizeRatio}
+>
 {#each images as image}
+<div class="item">
   <button class="poptoggle" popovertarget={image.SourceFile}>
-    <img src={image.SourceFile} alt="GTA San Andreas Multiplayer | SAMP Romania Roleplay" loading="lazy" width={image.ImageWidth} height={image.ImageHeight}/>
+    <img src={image.SourceFile} alt="GTA San Andreas Multiplayer | SAMP Romania Roleplay" loading="lazy"/>
   </button>
+</div>
 {/each}
-</section>
+</MasonryGrid>
+
 
 {#each images as image}
 <div popover class="container" id={image.SourceFile} use:toggle>
-    <img style="width: 100%" src={image.SourceFile} alt="GTA San Andreas Multiplayer | SAMP Romania Roleplay" loading="lazy"/>
+    <img src={image.SourceFile} alt="GTA San Andreas Multiplayer | SAMP Romania Roleplay" loading="lazy"/>
 </div>
 {/each}
 
@@ -42,32 +63,18 @@
 .container {
   border: 0;
   padding: 0;
-  max-width: 90%;
   line-height: 0;
 }
+
 .container::backdrop {
   background-color: rgba(0,0,0,0.75);
 }
 :global(body):has(:popover-open) button[popovertarget] {
   pointer-events: none;
 }
-#images-wrapper {    
-  line-height: 1;       
-  -webkit-column-count: 5;    
-  -webkit-column-gap: 3px;    
-  -moz-column-count: 5;
-  -moz-column-gap: 3px;
-  column-count: 5;
-  column-gap: 3px;
-}
-#images-wrapper img {    
-  width: 100% !important;    
-  height: auto !important;
-}
-#images-wrapper{    
-  display:inline-block;    
-  margin-right: auto;    
-  margin-left: auto;  
+img{
+  width: 350px;
+  height: auto
 }
 .poptoggle {
   border: none;
@@ -77,32 +84,5 @@
   background: none;
   width: 100%;
 }
-@media (max-width: 1200px) {
-  #images-wrapper {
-  -moz-column-count:    4;
-  -webkit-column-count: 4;
-  column-count:         4;
-  }
-}
-@media (max-width: 1000px) {
-  #images-wrapper {
-  -moz-column-count:    3;
-  -webkit-column-count: 3;
-  column-count:         3;
-  }
-}
-@media (max-width: 800px) {
-  #images-wrapper {
-  -moz-column-count:    2;
-  -webkit-column-count: 2;
-  column-count:         2;
-  }
-}
-@media (max-width: 400px) {
-  #images-wrapper {
-  -moz-column-count:    1;
-  -webkit-column-count: 1;
-  column-count:        1;
-  }
-}
+
 </style>
