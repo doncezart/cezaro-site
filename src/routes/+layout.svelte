f8f0f11a9fec2add193c780cd3e6a0119b62a4aa
cezar <doncezart>
2024-09-01 03:26:44 +0000

    Added analytics tracker

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,10 @@
     import '../clash-display.css'
 </script>
 
+<svelte:head>
+    <script defer src="https://analytics.ceza.ro/script.js" data-website-id="24c5bd27-2823-44b9-bc99-5037564d8cce"></script>
+</svelte:head>
+
 <div class="slot"><slot/></div>
 
 <style>
